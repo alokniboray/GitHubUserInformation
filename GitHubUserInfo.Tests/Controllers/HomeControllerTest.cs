7ea3134213feb70c30b7dcfaf9a075356db8fb6d
alokniboray <alok.niboray@gmail.com>
2016-04-08 16:53:05 +0530

    Added files via upload

---
+++ GitHubUserInfo.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using GitHubUserInfo.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitHubUserInfo.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            if (result != null)
+                Assert.AreEqual("GitHub User Information", result.ViewBag.Message);
+        }
+
+    
+    }
+}
