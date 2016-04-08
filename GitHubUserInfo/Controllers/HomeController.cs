dbed770b11299cdf473b08f9bff73517a674be00
alokniboray <alok.niboray@gmail.com>
2016-04-08 17:02:47 +0530

    Added files via upload

---
+++ GitHubUserInfo/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace GitHubUserInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "GitHub User Information";
+
+            return View();
+        }
+
+    }
+}
